43c1f4e496fd7a63ce7f4c07466a3152e8719d25
Amir Lanesman <amir.lanesman@xplenty.com>
2017-11-13 15:15:24 +0200

    fixes deployment script

--- xplenty-connections-config-deploy.sh
+++ xplenty-connections-config-deploy.sh
@@ -27,10 +27,12 @@ cp xplenty-connections-config.json xplenty-connections-config-${VERSION}.json
 echo "Deploying JSON configuration file"
 curl --show-error --fail -u ${ARTIFACTORY_USERNAME}:${ARTIFACTORY_PASSWORD} -X PUT "https://xplenty.jfrog.io/xplenty/${ENVIRONMENT}/com/xplenty/xplenty-connections-config/${VERSION}/" -T xplenty-connections-config-${VERSION}.json
 return_code=$?
+echo
 if [ "${return_code}" -eq "0" ]; then
   echo "Deploying POM file"
   curl --show-error --fail -u ${ARTIFACTORY_USERNAME}:${ARTIFACTORY_PASSWORD} -X PUT "https://xplenty.jfrog.io/xplenty/${ENVIRONMENT}/com/xplenty/xplenty-connections-config/${VERSION}/" -T xplenty-connections-config-${VERSION}.pom
   return_code=$?
+  echo
 fi
 
 echo "Removing temp files"
