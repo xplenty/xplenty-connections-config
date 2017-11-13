c34045f022dcd0dce6486d8057e71236820b1ac6
Amir Lanesman <amir.lanesman@xplenty.com>
2017-11-13 15:11:33 +0200

    fixes deployment script - adds logs

--- xplenty-connections-config-deploy.sh
+++ xplenty-connections-config-deploy.sh
@@ -6,8 +6,10 @@
 
 ENVIRONMENT=$1
 VERSION=$(cat ./version)
-echo "creating version ${VERSION}"
+echo "Creating version ${VERSION}"
 
+echo
+echo "Creating POM file"
 cat << EOF > xplenty-connections-config-${VERSION}.pom
 <?xml version="1.0" encoding="UTF-8"?><project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
   <modelVersion>4.0.0</modelVersion>
@@ -18,17 +20,22 @@ cat << EOF > xplenty-connections-config-${VERSION}.pom
 </project>
 EOF
 
+echo "Copying JSON configuration file"
 cp xplenty-connections-config.json xplenty-connections-config-${VERSION}.json
 
-# Upload GeoIPCountry to jfrog repository
+# Upload to jfrog repository
+echo "Deploying JSON configuration file"
 curl --show-error --fail -u ${ARTIFACTORY_USERNAME}:${ARTIFACTORY_PASSWORD} -X PUT "https://xplenty.jfrog.io/xplenty/${ENVIRONMENT}/com/xplenty/xplenty-connections-config/${VERSION}/" -T xplenty-connections-config-${VERSION}.json
 return_code=$?
 if [ "${return_code}" -eq "0" ]; then
+  echo "Deploying POM file"
   curl --show-error --fail -u ${ARTIFACTORY_USERNAME}:${ARTIFACTORY_PASSWORD} -X PUT "https://xplenty.jfrog.io/xplenty/${ENVIRONMENT}/com/xplenty/xplenty-connections-config/${VERSION}/" -T xplenty-connections-config-${VERSION}.pom
   return_code=$?
 fi
 
+echo "Removing temp files"
 rm xplenty-connections-config-${VERSION}.json
 rm xplenty-connections-config-${VERSION}.pom
 
+echo "Exiting"
 exit ${return_code}
